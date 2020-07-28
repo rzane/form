01edc1a0d464eadfe99c791fca323530e0490b8c
Ray Zane <raymondzane@gmail.com>
2020-07-28 14:13:55 -0400

    Remove unused types

--- src/types.ts
+++ src/types.ts
@@ -17,15 +17,6 @@ export type Touched<T> = T extends any[]
 export type Transform<T> = (value: T) => T;
 export type SetState<T> = (value: T | Transform<T>) => void;
 
-export interface NestedField<T> {
-  value: T;
-  error: ErrorMap<T> | undefined;
-  touched: TouchedMap<T> | undefined;
-  setValue: SetState<T>;
-  setError: SetState<ErrorMap<T> | undefined>;
-  setTouched: SetState<TouchedMap<T> | undefined>;
-}
-
 interface FieldState<T> {
   value: T;
   error: Errors<T> | undefined;
@@ -52,8 +43,11 @@ export interface Field<T> extends FieldState<T> {
   name: string;
 }
 
-type Filter<T, V> = {
-  [P in keyof T]: T[P] extends V ? P : never;
-};
-
-export type NamesOfType<T, V> = Filter<T, V>[keyof T];
+export interface NestedField<T> {
+  value: T;
+  error: ErrorMap<T> | undefined;
+  touched: TouchedMap<T> | undefined;
+  setValue: SetState<T>;
+  setError: SetState<ErrorMap<T> | undefined>;
+  setTouched: SetState<TouchedMap<T> | undefined>;
+}
