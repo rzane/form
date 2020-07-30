2aca789217f55fb16577dcde6d23ff1615a59250
Ray Zane <raymondzane@gmail.com>
2020-07-29 23:31:30 -0400

    Add test for new validation options

--- test/useForm.test.ts
+++ test/useForm.test.ts
@@ -1,6 +1,13 @@
 import { act, renderHook } from "@testing-library/react-hooks";
 import { useForm, FormOptions } from "../src";
 
+function makeValidator() {
+  return jest.fn().mockImplementation(() => ({
+    valid: false,
+    error: "invalid"
+  }));
+}
+
 function setup(options: Partial<FormOptions<string>> = {}) {
   return renderHook(() =>
     useForm({
@@ -63,4 +70,52 @@ test("changes `touched` with `setTouched`", () => {
 
   expect(result.current.touched).toEqual(true);
   expect(result.current.initialTouched).toBeUndefined();
+});
+
+describe("validateOnChange", () => {
+  test("does not run validations by default", () => {
+    const validate = makeValidator();
+    const { result } = setup({ validate });
+
+    act(() => result.current.setValue("changed"));
+    expect(validate).not.toHaveBeenCalled();
+  });
+
+  test("runs validations when enabled", async () => {
+    const validate = makeValidator();
+    const { result, waitForNextUpdate } = setup({
+      validate,
+      validateOnChange: true
+    });
+
+    act(() => result.current.setValue("changed"));
+    expect(validate).toHaveBeenCalledWith("changed");
+
+    await waitForNextUpdate();
+    expect(result.current.error).toEqual("invalid");
+  });
+});
+
+describe("validateOnBlur", () => {
+  test("does not run validations by default", () => {
+    const validate = makeValidator();
+    const { result } = setup({ validate });
+
+    act(() => result.current.setTouched(true));
+    expect(validate).not.toHaveBeenCalled();
+  });
+
+  test("runs validations when enabled", async () => {
+    const validate = makeValidator();
+    const { result, waitForNextUpdate } = setup({
+      validate,
+      validateOnBlur: true
+    });
+
+    act(() => result.current.setTouched(true));
+    expect(validate).toHaveBeenCalled();
+
+    await waitForNextUpdate();
+    expect(result.current.error).toEqual("invalid");
+  });
 });
